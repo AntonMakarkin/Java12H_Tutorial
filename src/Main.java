ac9ca4b740f0674b2661db0f6ae976baa825d586
AntonMakarkin <amakarkin27@gmail.com>
2026-05-04 21:50:21 +0300

    Part 3. User input

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,51 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I like pizza");
-        System.out.println("Its really good");
-        System.out.println("Buy me");
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter your name: ");
+//        String name = scanner.nextLine();
+//
+//        System.out.print("Enter your age: ");
+//        int age = scanner.nextInt();
+//
+//        System.out.print("What is your gpa: ");
+//        double gpa = scanner.nextDouble();
+//
+//        System.out.print("Are you student?");
+//        boolean isStudent = scanner.nextBoolean();
+//
+//        System.out.println("Hello " + name);
+//        System.out.println("You are " + age + " years old");
+//        System.out.println("Your gpa is: " + gpa);
+//
+//        if (isStudent) {
+//            System.out.println("You are enrolled as a student");
+//        } else {
+//            System.out.println("You are NOT enrolled");
+//        }
+//
+//        scanner.close();
 
-        int age = 21;
-        int year = 2025;
-        System.out.println(age);
-        System.out.println("The year is " + year);
+        // Calculate area of rectangle
 
-        double price = 19.99;
-        double gpa = 3.5;
-        double temperature = -12.5;
-        System.out.println("$" + price);
+        double width = 0;
+        double height = 0;
+        double area = 0;
 
-        char grade = 'A';
-        char symbol = '!';
-        System.out.println(grade);
+        Scanner recScanner = new Scanner(System.in);
 
-        boolean isStudent = true;
-        boolean forSale = false;
-        boolean isOnline = true;
+        System.out.print("Enter the width: ");
+        width = recScanner.nextDouble();
 
-        if (isStudent) {
-            System.out.println("You are a student");
-        } else {
-            System.out.println("You are not a student");
-        }
-        System.out.println(forSale);
+        System.out.print("Enter the height: ");
+        height = recScanner.nextDouble();
 
-        String name = "Anton Makarkin";
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age + " years old");
-        System.out.println("Your gpa is: " + gpa);
-        System.out.println("Your average letter grade is: " + grade);
+        area = width * height;
+
+        System.out.println("The area is: " + area + "cm");
+
+        recScanner.close();
     }
 }
