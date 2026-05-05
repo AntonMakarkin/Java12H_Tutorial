b4a17ce744d042cc0447c33b7984fc3ac40c0555
AntonMakarkin <amakarkin27@gmail.com>
2026-05-06 02:46:43 +0300

    mad libs game

--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.print("Enter your name: ");
-//        String name = scanner.nextLine();
-//
-//        System.out.print("Enter your age: ");
-//        int age = scanner.nextInt();
-//
-//        System.out.print("What is your gpa: ");
-//        double gpa = scanner.nextDouble();
-//
-//        System.out.print("Are you student?");
-//        boolean isStudent = scanner.nextBoolean();
-//
-//        System.out.println("Hello " + name);
-//        System.out.println("You are " + age + " years old");
-//        System.out.println("Your gpa is: " + gpa);
-//
-//        if (isStudent) {
-//            System.out.println("You are enrolled as a student");
-//        } else {
-//            System.out.println("You are NOT enrolled");
-//        }
-//
-//        scanner.close();
+        Scanner scanner = new Scanner(System.in);
 
-        // Calculate area of rectangle
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
 
-        double width = 0;
-        double height = 0;
-        double area = 0;
+        System.out.print("Enter an adjective (description): ");
+        adjective1 = scanner.nextLine();
+        System.out.print("Enter a noun (animal or person): ");
+        noun1 = scanner.nextLine();
+        System.out.print("Enter an adjective (description): ");
+        adjective2 = scanner.nextLine();
+        System.out.print("Enter a verb end with -ing (action): ");
+        verb1 = scanner.nextLine();
+        System.out.print("Enter an adjective (description): ");
+        adjective3 = scanner.nextLine();
 
-        Scanner recScanner = new Scanner(System.in);
 
-        System.out.print("Enter the width: ");
-        width = recScanner.nextDouble();
+        System.out.println("\nToday I went to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "!");
 
-        System.out.print("Enter the height: ");
-        height = recScanner.nextDouble();
-
-        area = width * height;
-
-        System.out.println("The area is: " + area + "cm");
-
-        recScanner.close();
+        scanner.close();
     }
 }
